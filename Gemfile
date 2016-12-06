1d61e68600e5af72daf399a10c22ea0338828a27
Peter Tascio <peter.tascio@gmail.com>
2016-12-06 10:10:59 -0500

    Write BE Auth methods for User/Add Bcrypt/Annotate/BetterErrors/BindingOfCaller to Gemfile

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'bcrypt'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -37,6 +37,9 @@ group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console'
   gem 'listen', '~> 3.0.5'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'annotate'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ActiveRecord::Base
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
+
+  attr_reader: password
+  after_intialize :ensure_session_token
+
+  def self.find_by_credentails(username, password)
+    user = User.find(params[:id])
+
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.gen_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.gen_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+    def ensure_session_token
+      self.session_token ||= User.gen_session_token
+    end
+
+end
