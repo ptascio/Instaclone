0041c1a1e4417fa1827a6f05fb36097289735172
Peter Tascio <peter.tascio@gmail.com>
2016-12-10 10:58:51 -0500

    Style posts page

--- frontend/components/post/post_index.jsx
+++ frontend/components/post/post_index.jsx
@@ -28,21 +28,31 @@ class PostIndex extends React.Component {
     const items = this.props.posts.map(function(post, index) {
       return (
         <li key={index}>
+          <article className="all-post post-holder pieko">
           <h3>{post.username}</h3>
-          <img src={post.image} height="200"/>
+            <div className="post-outside-container all-post _22yr2">
+              <div className="post-inner-container all-post">
+                <img src={post.image} className="post-image all-imgs"/>
+              </div>
+            </div>
           <button onClick={() => this.removePost(post)}>Delete</button>
+          </article>
         </li>
       );
     }.bind(this));
 
 
     return (
-      <div>
+      <main className="main-post main-section">
+      <section className="section-post column-holder">
+      <div className="all-post">
         <h1>This is posts index</h1>
         <ul>
           { items }
         </ul>
       </div>
+    </section>
+  </main>
     );
   }
 }
