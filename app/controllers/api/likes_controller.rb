6ae35e9b1982fbed0e1ed9c23c4e7267eb4fa75c
Peter Tascio <peter.tascio@gmail.com>
2016-12-14 09:26:18 -0500

    Create and destroy actions for Like

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,9 +1,37 @@
 class Api::LikesController < ApplicationController
 
   def create
+    @like = Like.new(
+      user_id: current_user.id,
+      post_id: like_params[:post_id]
+    )
+
+    if (@like.save)
+      render "api/likes/show", status: 200
+    else
+      @errors = like.errors.full_messages
+      #fix this! need an api/shared/error or similar
+      render "api/likes/show", status: 422
+    end
   end
 
-  def destroy 
+  def destroy
+    @like = Like.find_by(
+      user_id: current_user.id,
+      post_id: like_params[:post_id]
+    )
+
+    if(@like.destroy)
+      render "api/likes/show", status: 200
+    else
+      @errors = like.errors.full_messages
+      render "api/likes/show"
+    end
   end
+
+  private
+    def like_params
+      params.require(:like).permit(:post_id)
+    end
 
 end
