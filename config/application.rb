4968ad4edb8d9c731d4c7263cd3c7e01a35099d9
Peter Tascio <peter.tascio@gmail.com>
2016-12-14 21:41:40 -0500

    Another try to fix aws region

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Instaclone
     # -- all .rb files in that directory are automatically loaded.
     config.paperclip_defaults = {
      :storage => :s3,
-     :s3_region => ENV["s3_region"],
+     :s3_region => ENV['AWS_REGION'],
      :s3_credentials => {
        :bucket => ENV["s3_bucket"],
        :access_key_id => ENV["s3_access_key_id"],
